5ebe5e6f4e86592486aaa78ed061304d1f558a59
bravdar <darya.varb@list>
2022-06-12 09:35:17 +0300

    Initial Commit

---
+++ WordsCramble/WordsCrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsCrambleApp.swift
+//  WordsCramble
+//
+//  Created by Дарья Варб on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WordsCrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
